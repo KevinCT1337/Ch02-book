a9fc42ae0543386c8b156ba90426a99e0bf5cacb
Always Chaos <kevinmichocat1337@gmail.com>
2026-09-21 20:18:29 -0400

    Completed exercise 2.90

--- Book.java
+++ Book.java
@@ -110,7 +110,13 @@ class Book
      */
     public void setRefNumber(String ref)
     {
-        refNumber = ref;
+        if (ref.length() < 3){
+            System.out.println("ERROR: reference number must be at least 3 characters long!");
+        }
+        else
+        {
+            refNumber = ref;
+        }
     }
     
     /*
